938e96d87cd428b7ed90747c2eb820051eb0f0b1
muhuck <1562288394@qq.com>
2019-12-28 21:43:52 +0800

    完善注册登录

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,27 +18,40 @@
         </div>
         <!-- 注册弹出框 -->
         <el-dialog title="注册" width="450px" :visible.sync="register_dialog">
-          <el-form ref="form" :model="ruleForm" label-width="0" class="loginForm">
-              <el-form-item>
-                <el-input v-model="ruleForm.nickName" prefix-icon="el-icon-search"></el-input>
+          <el-form  :model="registerForm" ref="register_form" :rules="registerRule" label-width="0" class="loginForm">
+              <el-form-item prop="nickName">
+                <el-input v-model="registerForm.nickName" prefix-icon="iconfont icon-yonghu" placeholder="请输入昵称"></el-input>
               </el-form-item> 
-              <el-form-item>
-                <el-input v-model="ruleForm.email" prefix-icon="el-icon-search"></el-input>
+              <el-form-item prop="email">
+                <el-input v-model="registerForm.email" prefix-icon="iconfont icon-youxiang" type="email" placeholder="请输入邮箱"></el-input>
               </el-form-item> 
-              <el-form-item>
-                <el-input v-model="ruleForm.pass" prefix-icon="el-icon-search"></el-input>
+              <el-form-item prop="password">
+                <el-input v-model="registerForm.password" prefix-icon="iconfont icon-mima" type="password" placeholder="请输入密码"></el-input>
               </el-form-item> 
-              <el-form-item>
-                <el-input v-model="ruleForm.checkPass" prefix-icon="el-icon-search"></el-input>
+              <el-form-item prop="checkPass">
+                <el-input v-model="registerForm.checkPass" prefix-icon="iconfont icon-mima" type="password" placeholder="请确认密码"></el-input>
               </el-form-item> 
               <el-form-item class="btns">
-                  <el-button type="primary" @click="submitForm">立即创建</el-button>
-                  <el-button>取消</el-button>
+                  <el-button type="primary" @click="register">注册</el-button>
+                  <el-button type="primary" @click="setregisterForm">重置</el-button>
             </el-form-item>
           </el-form>
         </el-dialog>
         <!-- 登录弹出框 -->
-        <el-dialog title="登录" width="40%" :visible.sync="login_dialog"></el-dialog>
+        <el-dialog title="登录" width="450px" :visible.sync="login_dialog">
+            <el-form  :model="loginForm" ref="login_form" :rules="loginRule" label-width="0" class="loginForm">
+              <el-form-item prop="email">
+                <el-input v-model="loginForm.email" prefix-icon="iconfont icon-youxiang" type="email" placeholder="请输入邮箱"></el-input>
+              </el-form-item> 
+              <el-form-item prop="password">
+                <el-input v-model="loginForm.password" prefix-icon="iconfont icon-mima" type="password" placeholder="请输入密码"></el-input>
+              </el-form-item> 
+              <el-form-item class="btns">
+                  <el-button type="primary" @click="submitForm">登录</el-button>
+                  <el-button type="primary" @click="resetForm">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-dialog>
         <!-- 用户登录之后的个人信息区域 -->
         <div
           class="loginTrue"
@@ -91,12 +104,65 @@ import { mapMutations } from "vuex";
 // import { log } from 'util';
 export default {
   data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.registerForm.checkPass !== '') {
+            this.$refs.register_form.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.registerForm.password) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
     return {
-      ruleForm: {
+        // 表单数据对象
+      registerForm: {
         email: "",
         nickName: "",
-        pass: "",
+        password: "",
         checkPass: ""
+      },
+      loginForm:{
+          email:"",
+          password:"",
+      },
+    //  表单验证规则对象
+      registerRule:{
+         nickName: [
+            { required: true, message: '请输入昵称', trigger: 'blur' },
+            { min: 2, max: 5, message: '昵称长度在 2 到 5 个字符', trigger: 'blur' }
+          ],
+          email:[
+           { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+           { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+          ],
+            password: [
+            { validator: validatePass, trigger: 'blur' },
+             { min: 6, max: 16, message: '密码长度为6-16位字符', trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' },
+             { min: 6, max: 16, message: '密码长度为6-16位字符', trigger: 'blur' }
+          ]
+      },
+      loginRule:{
+          email:[
+           { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+           { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+          ],
+           password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+             { min: 6, max: 16, message: '密码长度为6-16位字符', trigger: 'blur' }
+          ],
       },
       userToken: "",
       isLogin: false,
@@ -109,18 +175,18 @@ export default {
   },
   methods: {
     ...mapMutations(["changeLogin"]),
-    submitForm(formName) {
+    submitForm() {
       // console.log(formName);
       let _this = this;
-      this.$refs[formName].validate(valid => {
+      this.$refs.login_form.validate(valid => {
         if (valid) {
-          // console.log(valid);
+          console.log(valid);
           this.axios({
             method: "post",
             url: "http://localhost:5000/api/login",
             data: {
-              email: this.ruleForm.email,
-              password: this.ruleForm.pass
+              email: this.loginForm.email,
+              password: this.loginForm.password
             },
             headers: {
               Anthorization: ""
@@ -130,21 +196,68 @@ export default {
             // console.log(this.userToken); //获取到的token
             _this.changeLogin({ Authorization: this.userToken });
             _this.isLogin = true
-            this.$router.push("/");
+            // this.$router.push("/");
             // $('#exampleModal2').modal('hide')
             // alert("登陆成功");
+            this.$message.success('登录成功')
+            this.login_dialog=false
           }).catch(error => {
+            this.$message.error('邮箱或密码错误，登录失败');
             console.log(error.response.data);
             
           })
         } else {
-          console.log("error submit!!");
+          this.$message.error('信息输入有误');
           return false;
         }
       });
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
+    register(){
+        // let _this = this;
+      this.$refs.register_form.validate(valid => {
+        if (valid) {
+          console.log(valid);
+          this.axios({
+            method: "post",
+            url: "http://localhost:5000/api/register",
+            data: {
+              email: this.registerForm.email,
+              nickName:this.registerForm.nickName,
+              password: this.registerForm.password
+            },
+            // headers: {
+            //   Anthorization: ""
+            // }
+          }).then(res => {
+            // _this.userToken = "Bearer" + res.data.token;
+            // // console.log(this.userToken); //获取到的token
+            // _this.changeLogin({ Authorization: this.userToken });
+            // _this.isLogin = true
+            const {data:result} = res
+            if(result.code==="1"){
+                this.$message.error('该邮箱已经注册！请登录');
+            }
+            else if(result.code==="2"){
+                this.$message.error('昵称已存在，请更换昵称')
+            }
+            else{
+                this.$message.success('注册成功')
+            }
+          }).catch(error => {
+            console.log(error)
+          })
+        } else {
+          this.$message.error('请正确填写表单数据')
+          return false;
+        }
+      });
+    },
+    resetForm() {
+        this.$refs.login_form.resetFields()
+    //   this.$refs[formName].resetFields();
+    },
+    setregisterForm(){
+        this.$refs.register_form.resetFields()
     },
     showDropdown() {
       this.dropdown = true;
@@ -187,7 +300,7 @@ body {
           padding: 5px 10px;
         }
       }
-      .logo {
+      .logo { 
         float: left;
         margin-top: 10px;
         img {
